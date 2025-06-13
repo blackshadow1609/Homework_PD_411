28bd6e643f0a10624c671d6a45b9cf23d3a5a679
Krestina_A <123819740+blackshadow1609@users.noreply.github.com>
2025-06-13 09:28:49 +0300

    Add files via upload

---
+++ Inheritance/Academy/Graduate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Graduate: Human
+	{
+		public string Speciality { get; set; }
+		public string Group { get; set; }
+		public string Graduate_work { get; set; }
+		public Graduate
+			(
+			string lastName, string firstName, int age,
+			string speciality, string group, string graduate_work
+			) : base(lastName, firstName, age)
+		{
+			Speciality = speciality;
+			Group = group;
+			Graduate_work = graduate_work;
+			Console.WriteLine($"TConstructor:\t {this.GetHashCode()}");
+		}
+
+		~Graduate()
+		{
+			Console.WriteLine($"TDestructor:\t {this.GetHashCode()}");
+		}
+
+		public void Info()
+		{
+			base.Info();
+			Console.WriteLine($"{Speciality} {Group} {Graduate_work}");
+		}
+	}
+}
---
+++ Inheritance/Academy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n--------------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student("Pinkman", "Jessie", 22, "Chemistry", "WW-220", 95, 96);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Info();
+			Console.WriteLine(delimiter);
+
+			Human tommy = new Human("Varcetty", "Tommy", 30);
+			tommy.Info();
+			Console.WriteLine(delimiter);
+
+			Student s_tommy = new Student(tommy, "Theft", "Vice", 95, 98);
+			s_tommy.Info();
+			Console.WriteLine(delimiter);
+
+			Graduate graduate = new Graduate("Pinkman", "Jessie", 35, "Chemistry", "WW-220", "Organic chemistry");
+			graduate.Info();
+			Console.WriteLine(delimiter); //Добавленный дипломник
+
+			Specialist specialist = new Specialist("Lomax", "Kevin", 30, "Lawyer", 10);
+			specialist.Info();
+			Console.WriteLine(delimiter); //Добавленный специалист
+		}
+	}
+}
